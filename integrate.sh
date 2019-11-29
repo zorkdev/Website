6f89d668ebfb5c5fa4082ac09ec77cc2ad9e8eb2
Attila Nemet <me@attilanemet.com>
2019-11-29 22:14:02 +0000

    Update integrate script

--- integrate.sh
+++ integrate.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-mint install yonaskolb/mint
+git clone https://github.com/yonaskolb/Mint.git
+cd Mint
+swift run mint install yonaskolb/mint
+cd ..
 mint run swiftlint
+swift test --generate-linuxmain
 swift test
 swift run
