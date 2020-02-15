97de14eba6c3fed9153cda3e0aa6d83091cf6ab0
Attila Nemet <me@attilanemet.com>
2020-02-15 14:00:07 +0000

    Update package

--- Package.swift
+++ Package.swift
@@ -7,9 +7,9 @@ let package = Package(
         .executable(name: "Website", targets: ["Website"])
     ],
     dependencies: [
-        .package(url: "https://github.com/JohnSundell/Publish.git", .upToNextMajor(from: "0.5.0")),
-        .package(url: "https://github.com/JohnSundell/SplashPublishPlugin", .upToNextMajor(from: "0.1.0")),
-        .package(url: "https://github.com/realm/SwiftLint.git", .upToNextMajor(from: "0.39.1"))
+        .package(url: "https://github.com/JohnSundell/Publish.git", from: "0.5.0"),
+        .package(url: "https://github.com/JohnSundell/SplashPublishPlugin", from: "0.1.0"),
+        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.39.1")
     ],
     targets: [
         .target(name: "Website", dependencies: ["Publish", "SplashPublishPlugin"])
