d9cfffe074bf25eedbf9c786c094c9d41498b11f
Attila Nemet <me@attilanemet.com>
2020-03-25 13:11:57 +0000

    Update to Swift 5.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
@@ -9,7 +9,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/JohnSundell/Publish.git", from: "0.5.0"),
         .package(url: "https://github.com/JohnSundell/SplashPublishPlugin", from: "0.1.0"),
-        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.39.1")
+        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.39.2")
     ],
     targets: [
         .target(name: "Website", dependencies: ["Publish", "SplashPublishPlugin"])
